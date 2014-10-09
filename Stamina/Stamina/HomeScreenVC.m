002fe568c1e83db649cc008d3a12241e987bb87e
Joao Lucas Sisanoski <joao.lbsisanoski@CAS418354M441.local>
2014-10-09 17:16:37 -0300

    Commit

---
+++ Stamina/Stamina/HomeScreenVC.m
@@ -0,0 +1,23 @@
+//
+//  HomeScreenVC.m
+//  Stamina
+//
+//  Created by Danilo Augusto Mative on 06/10/14.
+//  Copyright (c) 2014 Danilo Augusto Mative. All rights reserved.
+//
+
+#import "HomeScreenVC.h"
+
+@interface HomeScreenVC ()
+
+@end
+
+@implementation HomeScreenVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+@end
